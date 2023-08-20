4da28da7e0ca9c48d944dc132c598d7cf53dfb7c
Toufikur Rahman Toufik <toufikur.rahman@bjitgroup.com>
2023-08-20 13:30:29 +0600

    implement users interface and model

--- src/server.ts
+++ src/server.ts
@@ -3,16 +3,16 @@ import config from './config/index';
 import app from './app';
 
 async function connectDatabase() {
-  try {
-    await mongoose.connect(config.database_url as string);
-    console.log('Database successfully connected');
+    try {
+        await mongoose.connect(config.database_url as string);
+        console.log('Database successfully connected');
 
-    app.listen(config.port, () => {
-      console.log(`Application listening on port ${config.port}`);
-    });
-  } catch (err) {
-    console.log(err);
-  }
+        app.listen(config.port, () => {
+            console.log(`Application listening on port ${config.port}`);
+        });
+    } catch (err) {
+        console.log(err);
+    }
 }
 
 connectDatabase();
